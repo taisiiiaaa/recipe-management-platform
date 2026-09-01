36b2a651cb71fa0ac2daf23d16af469927c34b18
Taisiia Shloma <shloma20082004@icloud.com>
2026-09-02 01:27:48 +0200

    fix origin for deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://recipe-management-platform-eight.vercel.app/',
     credentials: true,
   });
 
