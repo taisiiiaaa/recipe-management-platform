7bfe5e02d8114155822be1be5802044106d8452e
Taisiia Shloma <shloma20082004@icloud.com>
2026-09-02 02:37:24 +0200

    fix registration DTO validation

--- backend/src/auth/dto/register-user.dto.ts
+++ backend/src/auth/dto/register-user.dto.ts
@@ -1,35 +1,21 @@
 import { IsEmail, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class RegisterUserDto {  
-    @ApiProperty()
-    username: string;
-  
-    @ApiProperty()
-    email: string;
-  
-    @ApiProperty()
-    @MinLength(6)
-    password: string;
-  
-    @ApiProperty()
-    confirmPassword: string;
+export class RegisterUserDto {
+  @ApiProperty()
+  @IsString()
+  username: string;
+
+  @ApiProperty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @ApiProperty()
+  @IsString()
+  confirmPassword: string;
 }
-
-// export class RegisterUserDto {
-//   @IsString()
-//   name: string;
-
-//   @IsString()
-//   username: string;
-
-//   @IsEmail()
-//   email: string;
-
-//   @IsString()
-//   @MinLength(6)
-//   password: string;
-
-//   @IsString()
-//   confirmPassword: string;
-// }
